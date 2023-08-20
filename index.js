5264d3bb9629d95c918ec3e82ae1a8ecb74ec4af
Phil Bladen <phil.bladen@gmail.com>
2023-08-20 14:19:06 +0100

    Functional HTML sound board with working pages.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-// Create a canvas element:
-let canvas = document.getElementById("canvas");
 let selectedPage = 0;
-// Create a context object, which contains all the methods for drawing objects:
-
 const keysDown = {};
 let renderPageOffsetX = 0;
 const audioCtx = new AudioContext();
+const keyboard = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+['z', 'x', 'c', 'v', 'b', 'n', 'm']];
+
+const mainColor = "#fcd140";
+const secondaryColor = "#2b2618";
 
 class AudioSample {
     constructor(name, buffer) {
@@ -20,6 +22,86 @@ class AudioSample {
         source.buffer = this.buffer;
         source.connect(audioCtx.destination);
         source.start(0);
+    }
+}
+
+class AudioSlot {
+    constructor(controlKey, parent, pageID) {
+        this.pageID = pageID;
+        this.controlKey = controlKey;
+
+        const div = document.createElement("div");
+        div.style = "display: flex; flex-direction: column; justify-content: center; align-items: center;"
+        div.style.minWidth = div.style.maxWidth = `${buttonSize - 10}px`;
+        div.style.minHeight = div.style.maxHeight = `${buttonSize - 10}px`;
+        div.style.borderRadius = "10px";
+        div.style.fontFamily = "HP";
+        div.style.fontSize = "25px";
+        div.style.textAlign = "center";
+        div.style.verticalAlign = "middle";
+        div.style.cursor = "pointer";
+        div.style.overflow = "hidden";
+        // div.style.backgroundColor = "#2b2618";
+
+        // div.style.border = "none";
+        // div.style.flexGrow = 1;
+        div.style.margin = "5px";
+        div.style.padding = "5px";
+        div.style.outline = "5px dashed #2b2618";
+        div.style.outlineOffset = "-5px";
+
+        const cross = document.getElementById("cross").cloneNode(true);
+        cross.id = "";
+        // cross.style.display = "";
+        div.appendChild(cross);
+
+        this.nameDiv = document.createElement("div");
+        this.nameDiv.style = "display: flex; flex-direction: column; justify-content: center; align-items: center; margin-bottom: 0px; flex: 1; overflow: hidden;"
+        this.nameDiv.style.textOverflow = "ellipsis";
+        // this.nameDiv.innerText = controlKey.toLocaleUpperCase();
+        div.appendChild(this.nameDiv);
+
+        this.div = div;
+
+        // Detect control modifier key press:
+        window.addEventListener("keydown", (e) => {
+            if (e.ctrlKey) {
+                cross.style.display = "";
+            }
+            // else {
+            // cross.style.display = "none";
+            // }
+        });
+        window.addEventListener("keyup", (e) => {
+            if (!e.ctrlKey) {
+                cross.style.display = "none";
+            }
+        });
+
+
+        // div.innerText = as.name;
+        const labelDiv = document.createElement("div");
+        labelDiv.style = "display: flex; flex-direction: column; justify-content: center; align-items: center; margin-bottom: 0px;";
+        labelDiv.innerText = controlKey.toLocaleUpperCase();
+        div.appendChild(labelDiv);
+
+        parent.appendChild(div);
+    }
+
+    loadFile(absoluteFilePath) {
+        if (!absoluteFilePath) return;
+        fs.readFile(absoluteFilePath, (err, data) => {
+            if (err) throw err;
+            const fileName = path.basename(absoluteFilePath);
+            const sampleName = fileName.substring(0, fileName.lastIndexOf("."));
+            audioCtx.decodeAudioData(toArrayBuffer(data), (buffer) => {
+                const as = new AudioSample(sampleName, buffer);
+                // this.div.style.backgroundColor = secondaryColor;
+                this.div.style.outlineStyle = "solid";
+                this.nameDiv.innerText = sampleName;
+                keyboardToSampleMap[this.pageID][this.controlKey] = as;
+            });
+        });
     }
 }
 
@@ -36,24 +118,13 @@ function toArrayBuffer(buffer) {
 }
 const audioDirectory = "P:\\House party\\Sound board\\HP Sound Board\\SoundBoard";
 const files = fs.readdirSync(audioDirectory);
+const validAudioFiles = [];
 for (const file of files) {
     if (fs.lstatSync(path.join(audioDirectory, file)).isDirectory()) continue;
-    let fileData = fs.readFileSync(path.join(audioDirectory, file), (err, data) => {
-        // if (err) throw err;
-        // sampleBuffer = data;
-    });
-    const sampleName = file.substring(0, file.lastIndexOf("."));
-    audioCtx.decodeAudioData(toArrayBuffer(fileData), (buffer) => {
-        audioSamples.push(new AudioSample(sampleName, buffer));
-    });
+    validAudioFiles.push(path.join(audioDirectory, file));
 }
 
-const keyboard = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-['z', 'x', 'c', 'v', 'b', 'n', 'm']];
 
-const mainColor = "#fcd140";
-const secondaryColor = "#2b2618";
 
 document.body.style.backgroundColor = mainColor;
 
@@ -61,49 +132,45 @@ const margin = 10;
 const buttonSize = ((window.innerWidth - margin) / keyboard[0].length) - margin;
 let i = 0;
 const app = document.createElement("div");
+app.id = "app";
 app.style = "display: flex; flex-direction: row; justify-content: center; align-items: center; width:100%; height:100%;";
 for (let pageIndex = 0; pageIndex < 3; pageIndex++) {
     const page = document.createElement("div");
-    page.style = "display: flex; flex-direction: column; justify-content: center; align-items: center; width:100%; height:100%;";
+    page.style = "display: flex; flex-direction: column; justify-content: center; align-items: center; width:100%; height:100%; position: absolute; transition: transform 0.15s ease-in-out;";
     for (const lineIndex in keyboard) {
         const row = document.createElement("div");
         row.style = "display: flex; flex-direction: row; justify-content: center; align-items: center;";
         const line = keyboard[lineIndex];
         for (const keyIndex in line) {
-            let as = audioSamples[++i];
-            if (!as) continue;
-            keyboardToSampleMap[pageIndex][line[keyIndex]] = as;
             const key = line[keyIndex];
-            const button = document.createElement("button");
-            button.style = "display: flex; flex-direction: column; justify-content: end; align-items: center;"
-            button.style.backgroundColor = secondaryColor;
-            button.style.color = mainColor;
-            button.style.border = "none";
-            button.style.borderRadius = "10px";
-            button.style.minWidth = `${buttonSize}px`;
-            // button.style.flexGrow = 1;
-            button.style.minHeight = `${buttonSize}px`;
-            button.style.margin = "5px";
-            button.style.fontFamily = "HP";
-            button.style.fontSize = "30px";
-            button.style.cursor = "pointer";
-            button.style.padding = 0;
-            button.innerText = as.name;
-            const labelDiv = document.createElement("div");
-            labelDiv.style = "display: flex; flex-direction: column; justify-content: center; align-items: center; margin-bottom: 10px;";
-            labelDiv.innerText = key.toLocaleUpperCase();
-            button.appendChild(labelDiv);
-            button.addEventListener("click", () => {
-                keyboardToSampleMap[selectedPage][key]?.play();
-            });
+            // if (validAudioFiles[i])/
+            new AudioSlot(key, row, pageIndex).loadFile(validAudioFiles[i++]);
+
+            // button.addEventListener("click", () => {
+            // keyboardToSampleMap[selectedPage][key]?.play();
+            // });
             // document.body.appendChild(button);
-            row.appendChild(button);
+            // row.appendChild(button);
         }
         page.appendChild(row);
     }
     app.appendChild(page);
 }
 document.body.appendChild(app);
+
+for (let pageIndicatorIndex = 0; pageIndicatorIndex < 3; pageIndicatorIndex++) {
+    const pageIndicator = document.createElement("div");
+    pageIndicator.style = `height: 30px; width: 30px; margin: 10px; border-radius: 999px; outline: 5px solid #2b2618; outline-offset: -5px; margin-bottom: 30px; ${pageIndicatorIndex == 0 ? `background-color: ${secondaryColor}` : mainColor}`;
+    pageIndicator.style.transition = "background-color 0.15s ease-in-out ";
+    document.getElementById("pageIndicators").appendChild(pageIndicator);
+}
+
+function updateIndicators() {
+    for (let pageIndicatorIndex = 0; pageIndicatorIndex < 3; pageIndicatorIndex++) {
+        const pageIndicator = document.getElementById("pageIndicators").children[pageIndicatorIndex];
+        pageIndicator.style.backgroundColor = selectedPage == pageIndicatorIndex ? secondaryColor : mainColor;
+    }
+}
 
 // function render() {
 //     let ctx = canvas.getContext("2d");
@@ -186,6 +253,12 @@ window.addEventListener("keydown", (e) => {
         keyboardToSampleMap[selectedPage][e.key.toLocaleLowerCase()]?.play();
 
     }
+
+    for (let i = 0; i < 3; i++) {
+        document.getElementById("app").children[i].style.transform = `translateX(${(i - selectedPage) * window.innerWidth}px)`;
+    }
+    // document.getElementById("app").children style.translateX = selectedPage * window.innerWidth;
+    updateIndicators();
 });
 window.addEventListener("keyup", (e) => {
     keysDown[e.key.toLocaleLowerCase()] = false;
